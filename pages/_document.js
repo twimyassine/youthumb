7f64e506dfc9256ea99f151ae495dbbdb4e76688
yassine naji <najiyassine069@gmail.com>
2023-09-02 19:30:09 +0100

    monitizin

--- pages/_document.js
+++ pages/_document.js
@@ -23,10 +23,8 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          <script>
-            (function(d){let s=d.createElement('script');s.async=true;s.src='https://kxshyo.com/code/silent.js?h=waWQiOjExNjQyNTIsInNpZCI6MTI2NTgyNywid2lkIjo0ODA0ODYsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);
-            </script>
-            <script>(function(d,a,b){let s=d.createElement('script');s.async=true;s.src='https://frenchequal.pro/code/pops.js?h=waWQiOjExNjQyNTIsInNpZCI6MTI2NTkyOSwid2lkIjo0ODA3MTQsInNyYyI6Mn0=eyJ&si1='+a+'&si2='+b;d.head.appendChild(s);})(document,'subid1','subid2');</script>        </Head>
+          <script>(function(d){let s=d.createElement('script');s.async=true;s.src='https://kxshyo.com/code/silent.js?h=waWQiOjExNjQyNTIsInNpZCI6MTI2NTgyNywid2lkIjo0ODA0ODYsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);</script>
+        </Head>
         <body>
           <Main />
           <NextScript />
